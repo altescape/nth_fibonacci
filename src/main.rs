ab25a847d600769f3743315dc3f629a5c0f1804c
altescape <hello@michaelwatts.me>
2022-03-22 13:37:31 +0000

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
         clearscreen::clear().unwrap();
 
-        let num: u64 = num.trim().parse().expect("A number is required");
+        let num: u32 = num.trim().parse().expect("A number is required");
 
         clearscreen::clear().unwrap();
 
@@ -20,29 +20,38 @@ fn main() {
 
         // Prompt continue or quit
         match Confirm::with_theme(&ColorfulTheme::default())
-        .with_prompt("Continue or quit?")
-        .default(true)
-        .wait_for_newline(true)
-        .interact_opt()
-        .unwrap()
-        {
-            Some(true) => {
-                clearscreen::clear().unwrap();
-                continue;
-            },
-            Some(false) => {
-                clearscreen::clear().unwrap();
-                break;
-            },
-            None => break,
-        }
+            .with_prompt("Continue or quit?")
+            .default(true)
+            .wait_for_newline(true)
+            .interact_opt()
+            .unwrap()
+            {
+                Some(true) => {
+                    clearscreen::clear().unwrap();
+                    continue;
+                },
+                Some(false) => {
+                    clearscreen::clear().unwrap();
+                    break;
+                },
+                None => break,
+            }
     }
 }
 
-fn next_fibonacci(n: u64) -> u64 {
+fn next_fibonacci(n: u32) -> u32 {
     if n <= 1 {
         return n;
     }
 
     return next_fibonacci(n - 1) + next_fibonacci(n - 2);
+}
+
+// This function returns the wrong value, @todo: see why
+fn fibonacci(n: u32) -> u32 {
+    match n {
+        0 => 1,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
 }
